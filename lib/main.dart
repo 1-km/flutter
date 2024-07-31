fc76f6ac0368ef401c71eb3e34b190ab89a7224a
DainJung <wjdekdls3693@gmail.com>
2024-07-31 17:21:15 +0900

    feat: 초기 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:military1km/login/login.dart';
-import 'package:military1km/signUp/sign_up.dart';
-import 'package:military1km/desc/start_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:military1km/app.dart';
+import 'package:military1km/common/data/preference/app_preferences.dart';
 
-void main() {
+void main() async {
+  // env 세팅
+  await dotenv.load(fileName: 'assets/config/.env');
+
+  // 내부저장소 세팅
+  await AppPreferences.init();
+  await Hive.initFlutter();
+
+  // gemini 세팅
+  Gemini.init(apiKey: dotenv.get("GEMINI_API"));
+
   runApp(
-    MaterialApp(
-      home: StartScreen(),
-    ),
+    const ProviderScope(child: App()),
   );
 }

656c5a29fc6bcad39291d314282726a6edc64d77
segye <tprpek@gmail.com>
2024-08-04 20:44:17 +0900

    email 인증 연동

---
+++ lib/data/entity/vo_signup.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'vo_signup.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserSignup _$UserSignupFromJson(Map<String, dynamic> json) => UserSignup(
+      email: json['email'] as String,
+      nickname: json['nickname'] as String,
+      military: json['military'] as String,
+      startdate: json['startdate'] as String,
+      finishdate: json['finishdate'] as String,
+      socialcode: json['socialcode'] as String,
+    );
+
+Map<String, dynamic> _$UserSignupToJson(UserSignup instance) =>
+    <String, dynamic>{
+      'email': instance.email,
+      'nickname': instance.nickname,
+      'military': instance.military,
+      'startdate': instance.startdate,
+      'finishdate': instance.finishdate,
+      'socialcode': instance.socialcode,
+    };
